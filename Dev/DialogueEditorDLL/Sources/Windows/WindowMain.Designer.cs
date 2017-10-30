a1e2e8a47ac5773e0dc53ef14c88a3190233a420
Sam Hocevar <sam@hocevar.net>
2017-10-30 16:54:54 +0100

    Do not embed WeifenLuo.WinFormsUI.Docking because it can be automatically retrieved via NuGet.

--- Dev/DialogueEditorDLL/Sources/Windows/WindowMain.Designer.cs
+++ Dev/DialogueEditorDLL/Sources/Windows/WindowMain.Designer.cs
@@ -639,7 +639,7 @@
             dockPaneStripToolWindowGradient1.InactiveTabGradient = tabGradient7;
             dockPaneStripSkin1.ToolWindowGradient = dockPaneStripToolWindowGradient1;
             dockPanelSkin1.DockPaneStripSkin = dockPaneStripSkin1;
-            this.dockPanel.Skin = dockPanelSkin1;
+            //this.dockPanel.Skin = dockPanelSkin1;
             this.dockPanel.TabIndex = 0;
             this.dockPanel.ActiveDocumentChanged += new System.EventHandler(this.OnActiveDocumentChanged);
             // 
